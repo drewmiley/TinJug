8fc88a1d36e231dc7d9d121a3f54c021e5d6f1e3
Drew Miley <dmiley@scottlogic.co.uk>
2017-03-11 12:15:24 +0000

    Pass- oneReturnsOne

--- src/main/java/GCD.java
+++ src/main/java/GCD.java
@@ -14,6 +14,12 @@ public class GCD {
         if (x < 0 || y < 0) {
             return NaN;
         }
+        if (x == 0 || y == 0) {
+            return 0;
+        }
+        if (x == 1 || y == 1) {
+            return 1;
+        }
         return 0;
     }
 }
